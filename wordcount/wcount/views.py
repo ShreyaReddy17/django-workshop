bf20f767830216d6c77f7dfe61ef61ff0b5f454b
shreya <shreyasanjub@gmail.com>
2019-01-19 12:21:41 +0530

    First project in github

---
+++ wordcount/wcount/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def homepage(request):
+    return render(request,'wcount/home.html')
+def about(request):
+    return render(request,'wcount/about.html')
+def count(request):
+    fulltext = request.GET['text']
+    word_count=len(fulltext.split())
+    word_dict={}
+    for w in fulltext.split():
+        if w in word_dict:
+            word_dict[w]+=1
+        else:
+            word_dict[w]=1
+    l=list(word_dict.items())
+    return render(request,'wcount/count.html',{'fulltext':fulltext,'word_count':word_count,'word_dict':word_dict,'l':l})
